360728b2b6b3c01642cd6563c828f1d7cf725d1c
Sweetbread654 <Sweetbread654@users.noreply.github.com>
2019-02-12 12:05:47 -0400

    added source file

---
+++ src/rdna.hpp
@@ -0,0 +1,17 @@
+#include <random>
+#include <iostream>
+using namespace std;
+
+char randDNA(int seed, string bases, int n)
+{
+	int n, i;
+	Random_device rd;
+	Mt19937 eng1 (rd()); // random seed
+	Mt19937 eng2 (seed);
+	
+	letters [] = {'C','G','A','T'};
+	
+	for (i=0, i<8, i++ 
+	
+	
+}
